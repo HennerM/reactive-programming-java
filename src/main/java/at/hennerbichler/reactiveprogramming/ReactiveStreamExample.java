0e7cfa4ef91603c3e033564f9005e3acec3dc5c8
Markus Hennerbichler <markushennerbichler@gmail.com>
2017-01-03 13:40:56 +0100

    Add ReactiveStreamExample

--- src/main/java/at/hennerbichler/reactiveprogramming/ReactiveStreamExample.java
+++ src/main/java/at/hennerbichler/reactiveprogramming/ReactiveStreamExample.java
@@ -1,7 +1,20 @@
 package at.hennerbichler.reactiveprogramming;
 
+import io.reactivex.Observable;
+
 /**
  * Created by markush on 1/2/17.
  */
 public class ReactiveStreamExample {
+
+    public static void main(String[] args) {
+
+        Observable<String> stringObservable = Observable.fromArray("abc", "d", "efg");
+        stringObservable
+                .map(String::toUpperCase)
+                .filter(value -> value.length() > 1)
+                .subscribe(System.out::println);
+
+    }
+
 }
